bf8f4deb592a899fe2672dbb82c5143072aac5e6
Jo Liss <joliss42@gmail.com>
2013-04-07 17:33:18 +0200

    Improve examples

--- redirector.rb
+++ redirector.rb
@@ -4,20 +4,19 @@ require 'sinatra'
 
 # GET /specific_path
 get '/specific_path' do
+  # Pass `, 301` for permanent redirects, but be warned: They can be cached
+  # permanently, so you cannot fix typos.
   redirect "http://new-site.example.com/new_path"
 end
 
 # GET /post/42
-get '/post/:id' do
-  redirect "http://new-site.example.com/blog/post/#{params[:id]}"
+get '/post/:id' do |post_id|
+  redirect "http://new-site.example.com/blog/post/#{post_id}"
 end
 
 # GET /anything
 get '*' do |path|
-  # REQUEST_URI includes the query string (?foo=bar).
-  full_path = env['REQUEST_URI']
-
-  # Pass `, 301` for permanent redirects, but be warned: They can be cached
-  # permanently, so you cannot fix typos.
-  redirect "http://new-site.example.com#{full_path}"
+  # Note: `path` does not include query strings. You can use
+  # env['REQUEST_URI'] etc. to get it.
+  redirect "http://new-site.example.com#{path}"
 end

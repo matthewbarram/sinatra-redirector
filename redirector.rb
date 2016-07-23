401c4b676bded084e66b0ca472365eb60b26b9dc
matthewbarram <matthewbarram@gmail.com>
2016-07-23 19:26:23 +1000

    new

--- redirector.rb
+++ redirector.rb
@@ -3,7 +3,12 @@ require 'sinatra'
 get '*' do |path|
   array_of_elements = env['REQUEST_URI'].split("/")
   puts array_of_elements
-  if array_of_elements.count == 3
+  @count = 0
+  array_of_elements.each do |item|
+    puts "#{item} - #{@count}"
+    @count = @count + 1
+  end
+  if array_of_elements.count == 4
     redirect "https://101geo.myshopify.com/blog/101geo/#{url}"
   elsif array_of_elements == 2 || array_of_elements.first == "tag"
     url = array_of_elements.last
